d2fd230e6e9971481427458833b8acc2e89676cd
Krittin Chuaiphikroh <krittin.c@email.kmutnb.ac.th>
2023-03-27 02:32:28 +0700

    kchuaiph: Init
    Navbar on mobile is still broken?
    
    Change-Id: I4049bb11e4c6997c2ef6d70874d301838e0040e1
    Signed-off-by: Krittin Chuaiphikroh <krittin.c@email.kmutnb.ac.th>

---
+++ src/kchuaiph/script.js
@@ -0,0 +1,49 @@
+let active = 'home';
+
+$('document').ready(() => {
+
+    const navitem = $('#nav').children()
+
+    navitem[0].className = 'active'
+
+    navitem[0].onclick = () => {
+        navitem[0].className = 'active'
+        navitem[1].className = ''
+        navitem[2].className = ''
+    }
+
+    navitem[1].onclick = () => {
+        navitem[0].className = ''
+        navitem[1].className = 'active'
+        navitem[2].className = ''
+    }
+
+    navitem[2].onclick = () => {
+        navitem[0].className = ''
+        navitem[1].className = ''
+        navitem[2].className = 'active'
+    }
+
+    window.addEventListener('scroll', () => {
+
+        if (window.scrollY < $('#about').offset().top) {
+            navitem[0].className = 'active'
+            navitem[1].className = ''
+            navitem[2].className = ''
+        }
+
+        if (window.scrollY >= $('#about').offset().top) {
+            navitem[0].className = ''
+            navitem[1].className = 'active'
+            navitem[2].className = ''
+        }
+
+        if (window.scrollY >= $('#contact').offset().top) {
+            navitem[0].className = ''
+            navitem[1].className = ''
+            navitem[2].className = 'active'
+        }
+
+    })
+
+})
