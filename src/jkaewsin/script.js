1ece9818dbf66458dd047cbba9792c19338f7ea3
Jirayu Kaewsing <46487993+StrixzIV@users.noreply.github.com>
2023-03-27 01:41:17 +0000

    Add contact section to navbar

--- src/jkaewsin/script.js
+++ src/jkaewsin/script.js
@@ -9,23 +9,39 @@ $('document').ready(() => {
     navitem[0].onclick = () => {
         navitem[0].className = 'active'
         navitem[1].className = ''
+        navitem[2].className = ''
     }
-
+    
     navitem[1].onclick = () => {
         navitem[0].className = ''
         navitem[1].className = 'active'
+        navitem[2].className = ''
     }
-
+    
+    navitem[2].onclick = () => {
+        navitem[0].className = ''
+        navitem[1].className = ''
+        navitem[2].className = 'active'
+    }
+    
     window.addEventListener('scroll', () => {
-
+        
         if (window.scrollY < $('#about').offset().top) {
             navitem[0].className = 'active'
             navitem[1].className = ''
+            navitem[2].className = ''
         }
-
-        else if (window.scrollY >= $('#about').offset().top) {
+        
+        else if (window.scrollY >= $('#about').offset().top && window.scrollY < $('#contact').offset().top) {
             navitem[0].className = ''
             navitem[1].className = 'active'
+            navitem[2].className = ''
+        }
+
+        else if (window.scrollY >= $('#contact').offset().top) {
+            navitem[0].className = ''
+            navitem[1].className = ''
+            navitem[2].className = 'active'
         }
 
     })
