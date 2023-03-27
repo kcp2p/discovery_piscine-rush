5ced63b9cffca810d64e107b3ce741bea386c1df
Krittin Chuaiphikroh <krittin.c@email.kmutnb.ac.th>
2023-03-27 14:44:19 +0700

    kchuaiph: Better errMsg
    Change-Id: Ie0e33f5eae78626cb6c473a43bd70a0782e0564e
    Signed-off-by: Krittin Chuaiphikroh <krittin.c@email.kmutnb.ac.th>

--- src/kchuaiph/init.js
+++ src/kchuaiph/init.js
@@ -1,14 +1,16 @@
 $('document').ready(() => {
 
+    const errMsg = "<h1><br>The current window width / height is not suitable to render this website.<br><br>Please readjust the width / height or use another device.</h1>"
+
     const bodyOnLoad = $('body').html()
     
     const checkUnavailable = (passWidth, passHeight) => {
         if (passWidth < 350 || passHeight < 640) { 
-            $('body').html('<h1>This client is not supported.</h1>')
+            $('body').html(errMsg)
             return
         }
 
-        if ($('body').html() === '<h1>This client is not supported.</h1>') {
+        if ($('body').html() === errMsg) {
             // set body to the saved body
                $('body').html(bodyOnLoad)        
         }
