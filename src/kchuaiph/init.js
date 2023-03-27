9b0ded54363621dda50ea9f11ba73e4e72c2fc98
Krittin Chuaiphikroh <krittin.c@email.kmutnb.ac.th>
2023-03-27 15:00:04 +0700

    kchuaiph: When life gives you JS, you guard it.
    Change-Id: I3ce9fc619e14dd82e8972dc914c028f65c2d50bf
    Signed-off-by: Krittin Chuaiphikroh <krittin.c@email.kmutnb.ac.th>

--- src/kchuaiph/init.js
+++ src/kchuaiph/init.js
@@ -3,16 +3,16 @@ $('document').ready(() => {
     const errMsg = "<h1><br>The current window width / height is not suitable to render this website.<br><br>Please readjust the width / height or use another device.</h1>"
 
     const bodyOnLoad = $('body').html()
-    
+
     const checkUnavailable = (passWidth, passHeight) => {
-        if (passWidth < 350 || passHeight < 640) { 
+        if (passWidth < 350 || passHeight < 640) {
             $('body').html(errMsg)
             return
         }
 
         if ($('body').html() === errMsg) {
             // set body to the saved body
-               $('body').html(bodyOnLoad)        
+               $('body').html(bodyOnLoad)
         }
     }
 
--- src/kchuaiph/script.js
+++ src/kchuaiph/script.js
@@ -2,9 +2,15 @@ let active = 'home';
 
 $('document').ready(() => {
 
-    if ($('body').html() === '<h1>This client is not supported.</h1>') {
-        return
+    const checkUnavailableHTML = () => {
+
+        if ($('body').html() === "<h1><br>The current window width / height is not suitable to render this website.<br><br>Please readjust the width / height or use another device.</h1>") {
+            return
+        }
+
     }
+
+    checkUnavailableHTML()
 
     const navitem = $('#nav').children()
 
@@ -15,27 +21,29 @@ $('document').ready(() => {
         navitem[1].className = ''
         navitem[2].className = ''
     }
-    
+
     navitem[1].onclick = () => {
         navitem[0].className = ''
         navitem[1].className = 'active'
         navitem[2].className = ''
     }
-    
+
     navitem[2].onclick = () => {
         navitem[0].className = ''
         navitem[1].className = ''
         navitem[2].className = 'active'
     }
-    
+
     window.addEventListener('scroll', () => {
-        
+
+        checkUnavailableHTML()
+
         if (window.scrollY < $('#about').offset().top) {
             navitem[0].className = 'active'
             navitem[1].className = ''
             navitem[2].className = ''
         }
-        
+
         else if (window.scrollY >= $('#about').offset().top && window.scrollY < $('#contact').offset().top) {
             navitem[0].className = ''
             navitem[1].className = 'active'
