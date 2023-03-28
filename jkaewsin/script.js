459d90f22c125993f3233bbbbd2c5f7885faa4f6
Jirayu Kaewsing <46487993+StrixzIV@users.noreply.github.com>
2023-03-28 02:31:21 +0000

    Change section navbar scroll position for better UX

--- jkaewsin/script.js
+++ jkaewsin/script.js
@@ -55,19 +55,19 @@ $('document').ready(() => {
 
     window.addEventListener('scroll', () => {
 
-        if (window.scrollY < $('#about').offset().top) {
+        if (window.scrollY < $('#about').offset().top && (window.scrollY < $('#about').offset().top - 100)) {
             navitem[0].className = 'active'
             navitem[1].className = ''
             navitem[2].className = ''
         }
 
-        else if (window.scrollY >= $('#about').offset().top && window.scrollY < $('#contact').offset().top - 10) {
+        else if (window.scrollY >= ($('#about').offset().top - 100) && window.scrollY < ($('#contact').offset().top - 50)) {
             navitem[0].className = ''
             navitem[1].className = 'active'
             navitem[2].className = ''
         }
 
-        else if (window.scrollY >= $('#contact').offset().top - 10) {
+        else if (window.scrollY >= $('#contact').offset().top - 50) {
             navitem[0].className = ''
             navitem[1].className = ''
             navitem[2].className = 'active'
