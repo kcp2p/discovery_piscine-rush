ba6bb080b6d3d29d897b9b188905d7674ad7b805
Jirayu Kaewsing <jirayukaewsing48@gmail.com>
2023-03-30 12:06:28 +0700

    Fix nav UX

--- jkaewsin/script.js
+++ jkaewsin/script.js
@@ -5,52 +5,48 @@ $('document').ready(() => {
     }
 
     const navitem = $('#nav').children()
-    let section = window.location.href.split('#')[1]
+    const contactButton = $('#contactBtn')
 
-    switch (section) {
-
-        case 'header':
-            navitem[0].className = 'active'
-            navitem[1].className = ''
-            navitem[2].className = ''
-            break
-
-        case 'about':
-            navitem[0].className = ''
-            navitem[1].className = 'active'
-            navitem[2].className = ''
-            break
-
-        case 'contact':
-            navitem[0].className = ''
-            navitem[1].className = ''
-            navitem[2].className = 'active'
-            break
-
-        default:
-            navitem[0].className = 'active'
-            navitem[1].className = ''
-            navitem[2].className = ''
-            break
-
-    }
+    contactButton.click(() => {
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $("#contact").offset().top
+        }, 50)
+    }) 
 
     navitem[0].onclick = () => {
+
         navitem[0].className = 'active'
         navitem[1].className = ''
         navitem[2].className = ''
+
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $("#header").offset().top
+        }, 50);
+
     }
 
     navitem[1].onclick = () => {
+
         navitem[0].className = ''
         navitem[1].className = 'active'
         navitem[2].className = ''
-    }
 
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $("#about").offset().top
+        }, 50)
+        
+    }
+    
     navitem[2].onclick = () => {
+        
         navitem[0].className = ''
         navitem[1].className = ''
         navitem[2].className = 'active'
+        
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $("#contact").offset().top
+        }, 50)
+    
     }
 
     window.addEventListener('scroll', () => {
